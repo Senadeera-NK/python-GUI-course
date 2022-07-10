6615b8b6841c6a65201babb935c80f83e5b91930
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-07-10 12:13:22 +0530

    2022/7/10

--- simple calculator.py
+++ simple calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from functools import partial
 
 #creating window
 window = Tk()
@@ -6,32 +7,90 @@ window = Tk()
 #window's title
 window.title("simple calculator")
 #window's width & height + x length + y height
-window.geometry("500x600+400+20")
+window.geometry("280x400+400+20")
 
 #display
-displayLabel = Label(window, text="display", bg="white", anchor="w", width=12, borderwidth=1, relief="solid", font=("Arial",50))
-displayLabel.place(x=10, y=8)
+displayEntry = Entry(window, text="display", bg="white", width=7, borderwidth=1, relief="solid", font=("Arial",50))
+displayEntry.place(x=8, y=8)
 
-#canvas all the button exists on the calculator
-canvas = Canvas(window, bg="grey", width=470, height= 480, borderwidth=1, relief="solid")
-canvas.place(x=10, y=100)
-
-#functions
 #functions
 def btnClicked(number):
-     displayLabel['text'] = number
+     displayEntry.insert(END, number)
+
+def btnAddFunction():
+    print("b")
+
+def btnMiniFunction():
+    print("c")
+
+def btnMultipleFunction():
+    print("d")
+
+def btnFunctionDelete():
+    displayEntry.delete(0, "end")
+
+def btnFunctionResult():
+    displayEntry.delete(0,"end")
+
+def btnFunctionDivide():
+    print("df")
+
+
+#canvas all the button exists on the calculator
+canvas = Canvas(window, bg="grey", width=250, height= 242, borderwidth=1, relief="solid")
+canvas.place(x=10, y=100)
 
 #all numbers buttons
-btn1 = Button(canvas, width=5, height=2, text="1", font=("Arial", 15), borderwidth=1, relief="solid", command=btnClicked(1))
+btn1 = Button(canvas, width=5, height=2, text="1", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "1"))
 btn1.place(x=3, y=3)
+#space between near x's -> 62
+btn2 = Button(canvas, width=5, height=2, text="2", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "2"))
+btn2.place(x=65, y=3)
 
-btn1 = Button(canvas, width=5, height=2, text="2", font=("Arial", 15), borderwidth=1, relief="solid", command=btnClicked(1))
-btn1.place(x=65, y=3)
+btn3 = Button(canvas, width=5, height=2, text="3", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "3"))
+btn3.place(x=127, y=3)
 
-btn1 = Button(canvas, width=5, height=2, text="3", font=("Arial", 15), borderwidth=1, relief="solid", command=btnClicked(1))
-btn1.place(x=127, y=3)
+#space between near y's -> 60
+btn4 = Button(canvas, width=5, height=2, text="4", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "4"))
+btn4.place(x=3, y=63)
 
-btn1 = Button(canvas, width=5, height=2, text="4", font=("Arial", 15), borderwidth=1, relief="solid", command=btnClicked(1))
-btn1.place(x=189, y=3)
+btn5 = Button(canvas, width=5, height=2, text="5", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "5"))
+btn5.place(x=65, y=63)
+
+btn6 = Button(canvas, width=5, height=2, text="6", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "6"))
+btn6.place(x=127, y=63)
+
+btn7 = Button(canvas, width=5, height=2, text="7", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "7"))
+btn7.place(x=3, y=123)
+
+btn8 = Button(canvas, width=5, height=2, text="8", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "8"))
+btn8.place(x=65, y=123)
+
+btn9 = Button(canvas, width=5, height=2, text="9", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "9"))
+btn9.place(x=127, y=123)
+
+btn0 = Button(canvas, width=5, height=2, text="0", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "0"))
+btn0.place(x=65, y=183)
+
+#mathematical functions
+btnAdd = Button(canvas, width=5, height=2, text="+", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "+"))
+btnAdd.place(x=189, y=3)
+
+btnMini = Button(canvas, width=5, height=2, text="-", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "-"))
+btnMini.place(x=189, y=63)
+
+btnMultiple = Button(canvas, width=5, height=2, text="x", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "x"))
+btnMultiple.place(x=189, y=123)
+
+btnEqual = Button(canvas, width=5, height=2, text="=", font=("Arial", 15), borderwidth=1, relief="solid", command = (btnFunctionResult))
+btnEqual.place(x=127, y=183)
+
+btnDivide = Button(canvas, width=5, height=2, text="/", font=("Arial", 15), borderwidth=1, relief="solid", command = partial(btnClicked, "/"))
+btnDivide.place(x=189, y=183)
+
+#delete
+btnDelete = Button(canvas, width=5, height=2, text="C", font=("Arial", 15), borderwidth=1, relief="solid", command = (btnFunctionDelete))
+btnDelete.place(x=3, y=183)
+
 
 window.mainloop()
